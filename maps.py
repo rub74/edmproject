247297fa91a5d9d2c18bb84682fd40f90fcf1b69
cé <40474549+sharkstronaut@users.noreply.github.com>
2024-06-24 17:47:29 +0200

    Update maps.py

--- maps.py
+++ maps.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import pydeck as pdk
 import altair as alt
-import matplotlib.pyplot as plt
 import pickle
 from datetime import datetime, timedelta
 from tqdm import tqdm
